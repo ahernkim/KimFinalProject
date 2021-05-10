07926be3d0cd6ab423357adb0d317fb064921010
ahernk1 <Snoop1988?>
2021-05-10 14:08:58 +0100

    Updates

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import warnings  ## Suppress warnings
 
-# set working directory
+# Set up working directory
 os.chdir("C:\\Users\\ahernk1\\OneDrive - Dell Technologies\\Desktop\\Data Analytics for Business Course")
 # print(os.getcwd())  # Print working directory
 # print(os.listdir(os.getcwd()))  ##Print all of the files in the working directory
 
+# Initial set up for importing Dataframe
 
 pd.set_option('display.max_columns', 20)  ## display max columns
 pd.set_option('display.width',
@@ -32,6 +33,7 @@ pp_2021 = pd.read_csv("PPR-2021-Cork.csv", index_col=0)  ##Read in Dataset. Set 
 # print(pp_2021.columns)  # to get the name of the columns
 # print(pp_2021.shape)  # Check shape of dataframe. Shape showing as (1403, 8)
 
+# Tidying up dataframe
 pp_2020_tidy = pp_2020.rename(columns={'Price (�)': 'Price'})  ##Rename column headings
 # print(pp_2020_tidy.columns)  ##verify new headings
 # print(pp_2020_tidy.shape)  # verify shape of dataframe
@@ -49,20 +51,22 @@ df_2020 = pp_2020_tidy.dropna(axis=1)  # dropping columns that contain NA values
 # print(pp_2020.shape, df_2020.shape) #check shape
 
 df_2021 = pp_2021_tidy.dropna(axis=1)  # dropping columns that contain NA values
-# print(pp_2021_tidy.shape, df_2021.shape) ##Shows shape after columns have been dropped. Now showing as (1403,6)
+#print(pp_2021_tidy.shape, df_2021.shape) ##Shows shape after columns have been dropped. Now showing as (1403,6)
 # print(df_2021.shape) #Verify shape
 
-# for index, row in dropcolumns.iterrows():  #Use Iterrows to loop over the DataFrame
-# print(index)
-# print(row)
-#   print(f'Data Sold: {index},Property Description:{row.values}')
+# Using Iterrows
+
+for index, row in pp_2021_tidy.iterrows():  #Use Iterrows to loop over the DataFrame
+    print(index)
+    print(row)
+print(f'Data Sold: {index},Property Description:{row.values}')
 
 price_df = df_2020[["Price"]]  # sort columns using indexing operator
 # print(check_df)
 
 print("The means for property prices for the year 2020")
 
-jan_df = price_df.loc["02/01/2020":"31/01/2020"]
+jan_df = price_df.loc["02/01/2020":"31/01/2020"]  #create a new dataframe loc.
 jan_df_mean = jan_df.mean()
 print("Januarys average house price was", jan_df.mean)
 feb_df = price_df.loc["03/02/2020":"29/02/2020"]
@@ -101,77 +105,130 @@ print("December average house price was", dec_df.mean())
 
 yr_review = pd.concat(
     [jan_df, feb_df, march_df, april_df, may_df, june_df, july_df, aug_df, sept_df, oct_df, nov_df, dec_df])
-print(yr_review)
+# print(yr_review)
+# print(yr_review.count())
 
 # Create a dictionary from the means.
 data_mean_20 = {'Means': [240564.786401, 255561.398529, 255593.268327, 251242.985103,
-                       257064.467909, 272570.637962, 245663.075518, 300831.322556, 266127.074665,
-                       259095.3477, 267788.692462, 318739.068602],
+                          257064.467909, 272570.637962, 245663.075518, 300831.322556, 266127.074665,
+                          259095.3477, 267788.692462, 318739.068602],
 
-             'Frame': ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Nov',
-                       'Dec']}
+                'Frame': ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+                          'Nov',
+                          'Dec']}
 
 # Make dataframe from dictionary
 data_df = pd.DataFrame(data_mean_20, columns=['Means', 'Frame'])
 
-print(data_df)
+# print(data_df)
 
 # Plot new dataframe
-#data_df.plot(x='Frame', y='Means')
-#plt.title('The means of property prices sold 2020')
-#plt.show()
+data_df.plot(x='Frame', y='Means')
+plt.title('The means of property prices sold 2020')
+plt.show()
 
-
-## Getting the means of property prices sold in 2021
+#Getting the means of property prices sold in 2021
 price_df21 = df_2021[["Price"]]  # sort columns using indexing operator
-#print(price_df21)
+# print(price_df21)
 
 print("The mean property prices for the year 2021")
 
 jan_df21 = price_df21.loc["04/01/2021":"31/01/2021"]
 jan_df21_mean = jan_df21.mean()
 print(jan_df21_mean)
-
-print("Januarys average house price was", jan_df21.mean)
+print("January 21 average house price was", jan_df21.mean)
 feb_df21 = price_df21.loc["01/02/2021":"26/02/2021"]
 feb_df21_mean = feb_df21.mean()
-print("February's average house price was", feb_df21.mean())
+print("February 21 average house price was", feb_df21.mean())
 march_df21 = price_df21.loc[("01/03/2021"):("31/03/2021")]
 march_df_mean = march_df21.mean()
-print("March's average house price was", march_df21.mean())
+print("March 21 average house price was", march_df21.mean())
 april_df21 = price_df21.loc[("01/04/2021"):("16/04/2021")]
 april_df_mean = april_df21.mean()
-print("Aprils average house price was", april_df21.mean())
+print("April 21 average house price was", april_df21.mean())
 
+# Create a dictionary from the means of the houses sold in 2021
 data_mean_21 = {'Means': [278353, 261664, 271134, 247189],
 
-             'Frame': ['Jan', 'Feb', 'March', 'April']}
+                'Frame': ['Jan', 'Feb', 'March', 'April']}
 
 # Make dataframe from dictionary
 data_df21 = pd.DataFrame(data_mean_21, columns=['Means', 'Frame'])
 
-print(data_df21)
+# print(data_df21) # check dataframe
 
 # Plot new dataframe
 data_df21.plot(x='Frame', y='Means')
 plt.title('The means of property prices sold 2021')
 plt.show()
 
+print("Working out the number houses sold per month in 2020..")
+
+print("The count is", jan_df.count())  ##Using count to check number of houses sold per month
+print("The count is", feb_df.count())
+print(print("The count is", march_df.count()))
+print("The count is", april_df.count())
+print("The count is", may_df.count())
+print("The count is", june_df.count())
+print("The count is", july_df.count())
+print("The count is", aug_df.count())
+print("The count is", sept_df.count())
+print("The count is", oct_df.count())
+print("The count is", nov_df.count())
+print("The count is", dec_df.count())
+
+# Create a dictionary from the number of houses sold.
+data_count_20 = {'Sold': [389, 401, 550, 290, 330, 373, 357, 260, 523, 574, 532, 787],
+
+                 'Frame': ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov',
+                           'Dec']}
+
+# Make dataframe from dictionary
+count_df = pd.DataFrame(data_count_20, columns=['Sold', 'Frame'])
+
+print(count_df)
+
+# Plot new dataframe
+count_df.plot(kind = 'scatter', x='Frame', y='Sold')
+plt.title('Number of houses sold in the months of 2020')
+plt.show()
 
 
+print("Reviewing how many houses were sold in 2021...")
+
+print("The count for Jan 21 is", jan_df21.count())
+print("The count for Feb 21", feb_df21.count())
+print(print("The count for March 21", march_df21.count()))
+print("The count for April 21", april_df21.count())
+
+# Create a dictionary from the count.
+data_count_21 = {'Sold': [357, 434, 505, 107],
+
+                 'Frame': ['Jan', 'Feb', 'March', 'April']}
+
+# Make dataframe from dictionary
+count_df21 = pd.DataFrame(data_count_21, columns=['Sold', 'Frame'])
+print(count_df21)
+
+# Plot new dataframe using scatter
+count_df21.plot(kind='scatter', x='Frame', y='Sold')
+plt.title('Number of houses sold in the months of 2021')
+plt.show()
 
 # Created a new dataset selecting only rows with New Dwelling.
+print("Reviewing New Dwelling Houses/Apartments sold in 2020 and 2021")
+
 description_2020 = df_2020[df_2020["Description of Property"] == "New Dwelling house /Apartment"]
 description_2021 = df_2021[df_2021["Description of Property"] == "New Dwelling house /Apartment"]
 
-#   print(description_2020.head()) # Check the first 5 rows
-#   print(description_2020.shape) ##Check the shape of my new dataset. Showing as (1192,6)
+print(description_2020.head())  # Check the first 5 rows
+print(description_2020.shape)  ##Check the shape of my new dataset. Showing as (1192,6)
 
-# print(description_2021.head())
-# print(description_2021.shape) ##Check the shape of my new dataset. Showing as (189,6)
+print(description_2021.head())
+print(description_2021.shape)  ##Check the shape of my new dataset. Showing as (189,6)
 
 merge = description_2020.append(description_2021)  ##Merged the two datasets together
-print(merge.head())
+# print(merge.head())
 # print(merge.tail())
 # print(merge.shape)  # shape now showing as (1381,6)
 
@@ -179,44 +236,45 @@ max_price = merge['Price'].max()
 print("The Maximum a new house or apartment was sold for in Cork between 2020 and 2021 was", max_price)
 min_price = merge['Price'].min()
 print("The Minimum a new house or apartment was sold for in Cork between 2020 and 2021 was", min_price)
-
 print("The Average House Price in 2020 was ", df_2020["Price"].mean())
 print("The Average House Price so far in 2021 is ", df_2021["Price"].mean())
 
 ## Create Numpy Array from house prices in December
 check_df = df_2020[["Price"]]  # sort columns using indexing operator
-print(check_df)
+# print(check_df)
 
 december_df = check_df.loc[("01/12/2020"):("31/12/2020")]  #
-#   print("Printing all houses sold in December 2020", first)
-# print(first.head())
-# print(check.shape)
-december_price = np.array(december_df[1:], dtype=float)  ##Create numpy array using prices from December
-# print(december_price)
+# print("Printing all houses sold in December 2020", december_df)
+# print(december_df.head())
+# print(december_df.shape)
+december_price_np = np.array(december_df[1:], dtype=float)  ##Create numpy array using prices from December
+print(december_price_np)
 
-december_mean = np.mean(december_price)
-december_min = np.min(december_price)
-december_max = np.max(december_price)
-print("The Average house sold in december 2020 was", december_mean)
-print("The Minimum house sold in december 2020 was", december_min)
-print("Maximum house sold in december 2020 was ", december_max)
+december_mean = np.mean(december_price_np)
+december_min = np.min(december_price_np)
+december_max = np.max(december_price_np)
 
-november_df = check_df.loc[("01/11/2020"):("30/11/2020")]  #
-# print("Printing all houses sold in November 2020", first)
-# print(check.shape)
+print("The Average a house sold for in December 2020 was", december_mean)
+print("The Minimum a house house sold in December 2020 was", december_min)
+print("The Maximum a house sold for in December 2020 was ", december_max)
+
+november_df = check_df.loc[
+              ("01/11/2020"):("30/11/2020")]  # find all houses listed from the start of November to the end
 
 ## Create Numpy Array from house prices in November
 november_price = np.array(november_df[1:], dtype=float)  ##Create numpy array using prices from December
-# print(november_price)
+
+print(november_price)
 november_mean = np.mean(november_price)
 november_min = np.min(november_price)
 november_max = np.max(november_price)
-print("The Average house sold in November 2020 was", november_mean)
-print("The Minimum house sold in November 2020 was", november_min)
-print("Maximum house sold in November 2020 was ", november_max)
+
+print("The Average a house sold for in November 2020 was", november_mean)
+print("The Minimum a house sold for in November 2020 was", november_min)
+print("The Maximum a house sold for in November 2020 was ", november_max)
 
 if november_max > december_max:
-    print("The month of November sold a more expensive house than december")
+    print("The month of November sold a more expensive house than December")
     print("It was", november_max - december_max, "more expensive")
 
 else:
@@ -224,7 +282,7 @@ else:
     print("It was", december_max - november_max, "more expensive")
 
 if november_min < december_min:
-    print("The month of November sold a cheaper house than december")
+    print("The month of November sold a cheaper house than December")
     print("It was", december_min - november_min, "more cheaper")
 
 else:
@@ -238,13 +296,6 @@ if november_mean > december_mean:
 else:
     print("December had higher average per house sold than November")
     print("It was", december_max - november_max, "higher")
-
-
-
-
-# np_property = df_2020[df_2020['Description of Property']].to_numpy()
-
-# print(df_2020.shape)
 
 # row = df_2020.iloc[4:8]
 # print(row)
